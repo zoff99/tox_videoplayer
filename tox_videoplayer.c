5464a844cbdd0f2c45721d4af32180baf2e68ac6
zoff99 <zoff99@users.noreply.github.com>
2023-05-13 20:19:19 +0200

    updates

--- tox_videoplayer.c
+++ tox_videoplayer.c
@@ -35,9 +35,11 @@
 #define _GNU_SOURCE
 
 #include <ctype.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 #include <libavutil/avutil.h>
@@ -56,8 +58,17 @@
 // include toxcore amalgamation with ToxAV --------
 
 
+#define DEFAULT_GLOBAL_AUD_BITRATE 32 // kbit/sec
+#define DEFAULT_GLOBAL_VID_BITRATE 8000 // kbit/sec
 
 static int self_online = 0;
+static int friend_online = 0;
+static pthread_t ffmpeg_thread;
+static int ffmpeg_thread_stop = 1;
+static const char *savedata_filename = "./savedata.tox";
+static const char *savedata_tmp_filename = "./savedata.tox.tmp";
+static char *global_decoder_string = "";
+static char *global_encoder_string = "";
 
 struct Node1 {
     char *ip;
@@ -71,9 +82,6 @@ struct Node1 {
 { "tox3.plastiras.org","4B031C96673B6FF123269FF18F2847E1909A8A04642BBECD0189AC8AEEADAF64",33445,3389},
     { NULL, NULL, 0, 0 }
 };
-
-const char *savedata_filename = "./savedata.tox";
-const char *savedata_tmp_filename = "./savedata.tox.tmp";
 
 static void hex_string_to_bin2(const char *hex_string, uint8_t *output)
 {
@@ -97,6 +105,25 @@ static void bin2upHex(const uint8_t *bin, uint32_t bin_size, char *hex, uint32_t
     for (size_t i = 0; i < hex_size - 1; i++) {
         hex[i] = toupper(hex[i]);
     }
+}
+
+// gives a counter value that increases every millisecond
+static uint64_t current_time_monotonic_default2(void)
+{
+    struct timespec clock_mono;
+    clock_gettime(CLOCK_MONOTONIC, &clock_mono);
+    uint64_t time = 1000ULL * clock_mono.tv_sec + (clock_mono.tv_nsec / 1000000ULL);
+    return time;
+}
+
+static void yieldcpu(uint32_t ms)
+{
+    usleep(1000 * ms);
+}
+
+static time_t get_unix_time(void)
+{
+    return time(NULL);
 }
 
 static void tox_log_cb__custom(Tox *tox, TOX_LOG_LEVEL level, const char *file, uint32_t line, const char *func,
@@ -142,15 +169,15 @@ static void friendlist_onConnectionChange(Tox *tox, uint32_t friend_number, TOX_
     switch (status) {
         case TOX_CONNECTION_NONE:
             printf("Lost connection to friend.\n");
-            self_online = 0;
+            friend_online = 0;
             break;
         case TOX_CONNECTION_TCP:
             printf("Connected to friend using TCP.\n");
-            self_online = 1;
+            friend_online = 1;
             break;
         case TOX_CONNECTION_UDP:
             printf("Connected to friend using UDP.\n");
-            self_online = 2;
+            friend_online = 2;
             break;
     }
 }
@@ -163,11 +190,41 @@ static void friend_request_cb(Tox *tox, const uint8_t *public_key, const uint8_t
 
 static void call_state_callback(ToxAV *av, uint32_t friend_number, uint32_t state, void *user_data)
 {
+    printf("ToxAV Call State change:fn=%d state=%d\n", friend_number, state);
 }
 
+static void call_callback(ToxAV *av, uint32_t friend_number, bool audio_enabled, bool video_enabled, void *user_data)
+{
+    printf("incoming ToxAV Call from fn=%d\n", friend_number);
+    toxav_answer(av, friend_number, DEFAULT_GLOBAL_AUD_BITRATE, DEFAULT_GLOBAL_VID_BITRATE, NULL);
+}
 
+#ifdef TOX_HAVE_TOXAV_CALLBACKS_002
+static void call_comm_callback(ToxAV *av, uint32_t friend_number, TOXAV_CALL_COMM_INFO comm_value,
+                                 int64_t comm_number, void *user_data)
+{
+    if (comm_value == TOXAV_CALL_COMM_DECODER_IN_USE_VP8)
+    {
+        global_decoder_string = " VP8 ";
+    }
+    else if (comm_value == TOXAV_CALL_COMM_DECODER_IN_USE_H264)
+    {
+        global_decoder_string = " H264";
+    }
+    else if (comm_value == TOXAV_CALL_COMM_ENCODER_IN_USE_VP8)
+    {
+        global_encoder_string = " VP8 ";
+        printf("ToxAV COMM: %s\n", global_encoder_string);
+    }
+    else if (comm_value == TOXAV_CALL_COMM_ENCODER_IN_USE_H264)
+    {
+        global_encoder_string = " H264";
+        printf("ToxAV COMM: %s\n", global_encoder_string);
+    }
+}
+#endif
 
-int main(int argc, char *argv[])
+static void *ffmpeg_thread_func(__attribute__((unused)) void *data)
 {
     AVFormatContext *format_ctx = NULL;
     AVCodecContext *audio_codec_ctx = NULL;
@@ -179,31 +236,22 @@ int main(int argc, char *argv[])
     AVFrame *frame = NULL;
     int audio_stream_index = -1;
     int video_stream_index = -1;
-    int got_frame;
     int num_samples;
     uint8_t **converted_samples = NULL;
     int ret;
 
-    if (argc < 2)
-    {
-        fprintf(stderr, "no input file specified\n");
-        return 0;
-    }
-
-    fprintf(stderr, "input file: %s\n", argv[1]);
-
-    av_register_all();
+    char *inputfile = (char *) data;
 
     // Open the input file
-    if ((ret = avformat_open_input(&format_ctx, argv[1], NULL, NULL)) < 0) {
-        fprintf(stderr, "Could not open input file '%s'\n", argv[1]);
-        return ret;
+    if ((ret = avformat_open_input(&format_ctx, inputfile, NULL, NULL)) < 0) {
+        fprintf(stderr, "Could not open input file '%s'\n", inputfile);
+        return NULL; // ret;
     }
 
     // Retrieve stream information
     if ((ret = avformat_find_stream_info(format_ctx, NULL)) < 0) {
         fprintf(stderr, "Could not find stream information\n");
-        return ret;
+        return NULL; // ret;
     }
 
     // Find the audio and video streams
@@ -218,15 +266,15 @@ int main(int argc, char *argv[])
             audio_codec_ctx = avcodec_alloc_context3(codec);
             if (!audio_codec_ctx) {
                 fprintf(stderr, "Could not allocate audio codec context\n");
-                return AVERROR(ENOMEM);
+                return NULL; // AVERROR(ENOMEM);
             }
             if ((ret = avcodec_parameters_to_context(audio_codec_ctx, codec_params)) < 0) {
                 fprintf(stderr, "Could not copy audio codec parameters to context\n");
-                return ret;
+                return NULL; // ret;
             }
             if ((ret = avcodec_open2(audio_codec_ctx, codec, NULL)) < 0) {
                 fprintf(stderr, "Could not open audio codec\n");
-                return ret;
+                return NULL; // ret;
             }
             audio_stream_index = i;
             audio_codec = codec;
@@ -234,15 +282,15 @@ int main(int argc, char *argv[])
             video_codec_ctx = avcodec_alloc_context3(codec);
             if (!video_codec_ctx) {
                 fprintf(stderr, "Could not allocate video codec context\n");
-                return AVERROR(ENOMEM);
+                return NULL; // AVERROR(ENOMEM);
             }
             if ((ret = avcodec_parameters_to_context(video_codec_ctx, codec_params)) < 0) {
                 fprintf(stderr, "Could not copy video codec parameters to context\n");
-                return ret;
+                return NULL; // ret;
             }
             if ((ret = avcodec_open2(video_codec_ctx, codec, NULL)) < 0) {
                 fprintf(stderr, "Could not open video codec\n");
-                return ret;
+                return NULL; // ret;
             }
             video_stream_index = i;
             video_codec = codec;
@@ -252,14 +300,14 @@ int main(int argc, char *argv[])
     // Make sure we found both audio and video streams
     if (audio_stream_index < 0 || video_stream_index < 0) {
         fprintf(stderr, "Could not find audio and video streams\n");
-        return AVERROR_EXIT;
+        return NULL; // AVERROR_EXIT;
     }
 
     // Allocate a frame for decoding
     frame = av_frame_alloc();
     if (!frame) {
         fprintf(stderr, "Could not allocate frame\n");
-        return AVERROR(ENOMEM);
+        return NULL; // AVERROR(ENOMEM);
     }
 
 
@@ -270,12 +318,12 @@ int main(int argc, char *argv[])
                                  0, NULL);
     if (!swr_ctx) {
         fprintf(stderr, "Could not allocate resampler context\n");
-        return 1;
+        return NULL; // 1;
     }
 
     if (swr_init(swr_ctx) < 0) {
         fprintf(stderr, "Could not initialize resampler context\n");
-        return 1;
+        return NULL; // 1;
     }
 
 
@@ -286,7 +334,7 @@ int main(int argc, char *argv[])
             video_codec_ctx->width, video_codec_ctx->height, 1));
     if (yuv_buffer == NULL) {
         fprintf(stderr, "Error: could not allocate YUV buffer\n");
-        return 1;
+        return NULL; // 1;
     }
 
     uint8_t *dst_yuv_buffer[3];
@@ -301,17 +349,129 @@ int main(int argc, char *argv[])
 
     if (scaler_ctx == NULL) {
         fprintf(stderr, "Error: could not create scaler context\n");
-        return 1;
+        return NULL; // 1;
     }
 
     fprintf(stderr, "SwsContext: %dx%d\n", video_codec_ctx->width, video_codec_ctx->height);
 
 
+    while (friend_online == 0)
+    {
+        yieldcpu(100);
+    }
+
+    // Read packets from the input file and decode them
+    while ((av_read_frame(format_ctx, &packet) >= 0) && (ffmpeg_thread_stop != 1))
+    {
+        if (packet.stream_index == audio_stream_index)
+        {
+            // Decode audio packet
+            ret = avcodec_send_packet(audio_codec_ctx, &packet);
+            if (ret < 0)
+            {
+                fprintf(stderr, "Error sending audio packet for decoding\n");
+                break;
+            }
+
+            while (ret >= 0)
+            {
+               ret = avcodec_receive_frame(audio_codec_ctx, frame);
+                if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
+                {
+                    break;
+                }
+                else if (ret < 0)
+                {
+                    fprintf(stderr, "Error during audio decoding\n");
+                    break;
+                }
+
+                num_samples = swr_get_out_samples(swr_ctx, frame->nb_samples);
+                av_samples_alloc_array_and_samples(&converted_samples, NULL, 2, num_samples, AV_SAMPLE_FMT_S16, 0);
+                swr_convert(swr_ctx, converted_samples, num_samples, (const uint8_t **)frame->extended_data, frame->nb_samples);
+
+                // Do something with the converted samples here
+                int64_t pts = frame->pts;
+                // fprintf(stderr, "AudioFrame:fn:%10d pts:%10ld sr:%5d ch:%1d samples:%3d\n",
+                //     audio_codec_ctx->frame_number, pts, frame->sample_rate, 2, num_samples);
+
+                av_freep(&converted_samples[0]);
+                av_freep(&converted_samples);
+            }
+        }
+        else if (packet.stream_index == video_stream_index)
+        {
+            // Decode video packet
+            ret = avcodec_send_packet(video_codec_ctx, &packet);
+            if (ret < 0)
+            {
+                fprintf(stderr, "Error sending video packet for decoding\n");
+                break;
+            }
+
+            while (ret >= 0)
+            {
+                ret = avcodec_receive_frame(video_codec_ctx, frame);
+                if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
+                {
+                    break;
+                } else if (ret < 0)
+                {
+                    fprintf(stderr, "Error during video decoding\n");
+                    break;
+                }
+
+                // Convert the video frame to YUV
+                int planes_stride[3];
+                planes_stride[0] = av_image_get_linesize(AV_PIX_FMT_YUV420P, video_codec_ctx->width, 0);
+                planes_stride[1] = av_image_get_linesize(AV_PIX_FMT_YUV420P, video_codec_ctx->width, 1);
+                planes_stride[2] = av_image_get_linesize(AV_PIX_FMT_YUV420P, video_codec_ctx->width, 2);
+                // fprintf(stderr, "VideoFrame:strides:%d %d %d\n",planes_stride[0],planes_stride[1],planes_stride[2]);
+
+                sws_scale(scaler_ctx, (const uint8_t * const*)frame->data, frame->linesize, 0, video_codec_ctx->height,
+                        dst_yuv_buffer, planes_stride);
+
+                int64_t pts = frame->pts;
+                // fprintf(stderr, "VideoFrame:ls:%d %dx%d fn:%10d pts:%10ld pn:%10d\n",
+                //             frame->linesize[0], frame->width, frame->height,
+                //             video_codec_ctx->frame_number, pts, frame->coded_picture_number);
+
+                av_frame_unref(frame);
+            }
+        }
+        av_packet_unref(&packet);
+        yieldcpu(1000 / 30);
+    }
+
+    // Clean up
+    ffmpeg_thread = 1;
+    pthread_join(ffmpeg_thread, NULL);
+    //
+    av_frame_free(&frame);
+    avcodec_free_context(&audio_codec_ctx);
+    avcodec_free_context(&video_codec_ctx);
+    avformat_close_input(&format_ctx);
+    av_free(yuv_buffer);
+    swr_free(&swr_ctx);
+    sws_freeContext(scaler_ctx);
+
+    printf("ffmpeg Thread:Clean thread exit!\n");
+    return NULL;
+}
 
 
 
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        fprintf(stderr, "no input file specified\n");
+        return 0;
+    }
 
+    fprintf(stderr, "input file: %s\n", argv[1]);
 
+    av_register_all();
 
 
 
@@ -352,6 +512,9 @@ int main(int argc, char *argv[])
     }
 
 
+    self_online = 0;
+    friend_online = 0;
+
 #ifndef TOX_HAVE_TOXUTIL
     printf("init Tox\n");
     Tox *tox = tox_new(&options, NULL);
@@ -361,6 +524,9 @@ int main(int argc, char *argv[])
 #endif
     printf("init ToxAV\n");
     ToxAV *toxav = toxav_new(tox, NULL);
+
+    updateToxSavedata(tox);
+
     // ----- CALLBACKS -----
 #ifdef TOX_HAVE_TOXUTIL
     tox_utils_callback_self_connection_status(tox, self_connection_change_callback);
@@ -371,7 +537,15 @@ int main(int argc, char *argv[])
     tox_callback_self_connection_status(tox, self_connection_change_callback);
 #endif
     tox_callback_friend_request(tox, friend_request_cb);
+
     toxav_callback_call_state(toxav, call_state_callback, NULL);
+    toxav_callback_call(toxav, call_callback, NULL);
+#ifdef TOX_HAVE_TOXAV_CALLBACKS_002
+    printf("have toxav_callback_call_comm\n");
+    toxav_callback_call_comm(toxav, call_comm_callback, NULL);
+#endif
+
+
     // ----- CALLBACKS -----
     // ----- bootstrap -----
     printf("Tox bootstrapping\n");
@@ -409,7 +583,7 @@ int main(int argc, char *argv[])
     while (1 == 1)
     {
         tox_iterate(tox, NULL);
-        usleep(tox_iteration_interval(tox));
+        yieldcpu(tox_iteration_interval(tox));
         if (self_online > 0)
         {
             break;
@@ -420,90 +594,37 @@ int main(int argc, char *argv[])
 
 
 
-
-
-
-
-
-
-
-
-    // Read packets from the input file and decode them
-    while (av_read_frame(format_ctx, &packet) >= 0) {
-        if (packet.stream_index == audio_stream_index) {
-            // Decode audio packet
-            ret = avcodec_send_packet(audio_codec_ctx, &packet);
-            if (ret < 0) {
-                fprintf(stderr, "Error sending audio packet for decoding\n");
-                break;
-            }
-
-            while (ret >= 0) {
-               ret = avcodec_receive_frame(audio_codec_ctx, frame);
-                if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF) {
-                    break;
-                } else if (ret < 0) {
-                    fprintf(stderr, "Error during audio decoding\n");
-                    break;
-                }
-
-                num_samples = swr_get_out_samples(swr_ctx, frame->nb_samples);
-                av_samples_alloc_array_and_samples(&converted_samples, NULL, 2, num_samples, AV_SAMPLE_FMT_S16, 0);
-                swr_convert(swr_ctx, converted_samples, num_samples, (const uint8_t **)frame->extended_data, frame->nb_samples);
-
-                // Do something with the converted samples here
-                int64_t pts = frame->pts;
-                fprintf(stderr, "AudioFrame:fn:%10d pts:%10ld sr:%5d ch:%1d samples:%3d\n",
-                    audio_codec_ctx->frame_number, pts, frame->sample_rate, 2, num_samples);
-
-                av_freep(&converted_samples[0]);
-                av_freep(&converted_samples);
-            }
-        } else if (packet.stream_index == video_stream_index) {
-            // Decode video packet
-            ret = avcodec_send_packet(video_codec_ctx, &packet);
-            if (ret < 0) {
-                fprintf(stderr, "Error sending video packet for decoding\n");
-                break;
-            }
-            while (ret >= 0) {
-                ret = avcodec_receive_frame(video_codec_ctx, frame);
-                if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF) {
-                    break;
-                } else if (ret < 0) {
-                    fprintf(stderr, "Error during video decoding\n");
-                    break;
-                }
-
-                // Convert the video frame to YUV
-                int planes_stride[3];
-                planes_stride[0] = av_image_get_linesize(AV_PIX_FMT_YUV420P, video_codec_ctx->width, 0);
-                planes_stride[1] = av_image_get_linesize(AV_PIX_FMT_YUV420P, video_codec_ctx->width, 1);
-                planes_stride[2] = av_image_get_linesize(AV_PIX_FMT_YUV420P, video_codec_ctx->width, 2);
-                // fprintf(stderr, "VideoFrame:strides:%d %d %d\n",planes_stride[0],planes_stride[1],planes_stride[2]);
-
-                sws_scale(scaler_ctx, (const uint8_t * const*)frame->data, frame->linesize, 0, video_codec_ctx->height,
-                        dst_yuv_buffer, planes_stride);
-
-                int64_t pts = frame->pts;
-                fprintf(stderr, "VideoFrame:ls:%d %dx%d fn:%10d pts:%10ld pn:%10d\n",
-                            frame->linesize[0], frame->width, frame->height,
-                            video_codec_ctx->frame_number, pts, frame->coded_picture_number);
-
-                av_frame_unref(frame);
-            }
-        }
-        av_packet_unref(&packet);
+    ffmpeg_thread_stop = 0;
+    if (pthread_create(&ffmpeg_thread, NULL, ffmpeg_thread_func, (void *)argv[1]) != 0)
+    {
+        printf("ffmpeg Thread create failed\n");
+    }
+    else
+    {
+        pthread_setname_np(ffmpeg_thread, "t_notif");
+        printf("ffmpeg Thread successfully created\n");
     }
 
+
+    // ----------- main loop -----------
+    while (1 == 1)
+    {
+        tox_iterate(tox, NULL);
+        yieldcpu(2);
+        toxav_iterate(toxav);
+        yieldcpu(2);
+    }
+    // ----------- main loop -----------
+
+
+
+
+
+
+
     // Clean up
-    av_frame_free(&frame);
-    avcodec_free_context(&audio_codec_ctx);
-    avcodec_free_context(&video_codec_ctx);
-    avformat_close_input(&format_ctx);
-    av_free(yuv_buffer);
-    swr_free(&swr_ctx);
-    sws_freeContext(scaler_ctx);
+    ffmpeg_thread = 1;
+    pthread_join(ffmpeg_thread, NULL);
 
     toxav_kill(toxav);
     printf("killed ToxAV\n");
