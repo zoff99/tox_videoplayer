382cadf5066fc2ce737d5fce5f48985a4749a96f
zoff99 <zoff99@users.noreply.github.com>
2023-05-21 12:49:36 +0200

    add correct license into source file

--- tox_videoplayer.c
+++ tox_videoplayer.c
@@ -10,12 +10,12 @@
 /*
  * Copyright © 2023 Zoff <zoff@zoff.cc>
  *
- * Tox is free software: you can redistribute it and/or modify
+ * tox_videoplayer is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Tox is distributed in the hope that it will be useful,
+ * tox_videoplayer is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
